290a7ad280defe8320e756aa728edd87b214c620
Issa Rice <riceissa@gmail.com>
2018-01-24 11:56:56 -0800

    get the sub-area

--- proc_table.py
+++ proc_table.py
@@ -4,7 +4,7 @@ import re
 from bs4 import BeautifulSoup
 
 
-FILE_PATHS = ["data/1991-health.html"]
+FILE_PATHS = ["data/1991-environment.html"]
 
 
 def main():
@@ -16,15 +16,25 @@ def main():
                 num_col = len(trs[1].find_all("td"))
                 if num_col != 5:
                     continue
-                label = table.previous_sibling.text
+                label = sub_area(table)
+                print(label)
                 for tr in trs:
                     cols = tr.find_all("td")
                     try:
                         location = cleaned(cols[0].find("i").text)
                     except:
                         location = ""
-                    print(cleaned(label), location,
+                    print(label, location,
                           list(map(lambda x: cleaned(x.text), cols)))
+
+
+def sub_area(table):
+    """Find the sub-area of the grant. These are given right above the table in
+    bold."""
+    tag = table.previous_sibling
+    while tag is not None and tag.find("b") in [None, -1]:
+        tag = tag.previous_sibling
+    return cleaned(tag.find("b").text)
 
 
 def cleaned(s):
