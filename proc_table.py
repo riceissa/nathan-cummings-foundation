ee05d6a9433b50f97886e6bcef9728eaffcb97d8
Issa Rice <riceissa@gmail.com>
2018-01-24 15:30:29 -0800

    add filepaths

--- proc_table.py
+++ proc_table.py
@@ -4,7 +4,38 @@ import re
 from bs4 import BeautifulSoup
 
 
-FILE_PATHS = ["data/1991-environment.html"]
+FILE_PATHS = [
+    "data/1991-arts.html",
+    "data/1991-community.html",
+    "data/1991-environment.html",
+    "data/1991-health.html",
+    "data/1991-interprogram.html",
+    "data/1991-jewishlife.html",
+    "data/1992-arts.html",
+    "data/1992-community.html",
+    "data/1992-environment.html",
+    "data/1992-health.html",
+    "data/1992-interprogram.html",
+    "data/1992-jewishlife.html",
+    "data/1992-presidential.html",
+    "data/1992-prior.html",
+    "data/1992-researchdevelopmentevaluation.html",
+    "data/1993-arts.html",
+    "data/1993-community.html",
+    "data/1993-environment.html",
+    "data/1993-health.html",
+    "data/1993-interprogram.html",
+    "data/1993-jewishlife.html",
+    "data/1993-presidential.html",
+    "data/1993-researchdevelopmentevaluation.html",
+    "data/1994-arts.html",
+    "data/1994-community.html",
+    "data/1994-environment.html",
+    "data/1994-health.html",
+    "data/1994-interprogram.html",
+    "data/1994-jewishlife.html",
+    "data/1994-presidential.html",
+]
 
 
 def main():
@@ -13,7 +44,11 @@ def main():
             soup = BeautifulSoup(f, "lxml")
             for table in soup.find_all("table"):
                 trs = table.find_all("tr")
-                num_col = len(trs[1].find_all("td"))
+                try:
+                    num_col = len(trs[1].find_all("td"))
+                except IndexError:
+                    # This is a table with just one row, so skip it
+                    continue
                 if num_col != 5:
                     continue
                 label = sub_area(table)
@@ -34,7 +69,9 @@ def sub_area(table):
     tag = table.previous_sibling
     while tag is not None and tag.find("b") in [None, -1]:
         tag = tag.previous_sibling
-    return cleaned(tag.find("b").text)
+    if tag:
+        return cleaned(tag.find("b").text)
+    return ""
 
 
 def cleaned(s):
