a30ae3faa6b14b0f6493db707e1b9dd1fbb1ff19
Issa Rice <riceissa@gmail.com>
2018-01-26 18:01:29 -0800

    ignore the h2

--- proc_list.py
+++ proc_list.py
@@ -98,6 +98,9 @@ def write_grant(grant, sub_area, file_path, writer):
     year = int(file_path[len("data/"):len("data/YYYY")])
     if grant == (['\n'], None, []):
         pass
+    elif (any(map(lambda x: x.name == "h2", grant[0])) and
+        grant[1] is None and grant[2] == []):
+        pass
     elif not grantee:
         print(file_path, grant, file=sys.stderr)
     else:
